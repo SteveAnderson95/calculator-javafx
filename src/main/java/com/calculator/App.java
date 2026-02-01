3b6a43e3585d65dcc0f42ae0efcd5468bd1b0e6f
steve-anderson <steve.anderson.hakiz25@gmail.com>
2026-02-01 23:26:06 +0100

    Initial commit

---
+++ src/main/java/com/calculator/App.java
@@ -0,0 +1,17 @@
+package com.calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+    @Override
+    public void start(Stage primaryStage) {
+
+
+    }
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+}
